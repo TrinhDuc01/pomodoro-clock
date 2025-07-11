3611848328e512b069ec94504e5d40a2c8a9859f
TrinhDuc01 <ductrinh.0301@gmail.com>
2025-07-11 12:17:47 +0700

    BarTiming

--- src/components/BarTiming.jsx
+++ src/components/BarTiming.jsx
@@ -1,6 +1,7 @@
 const BarTiming = () => {
-    return <div>
-        BarTiming
+    return <div className="relative mb-8">
+        <div className="bg-gray-950 opacity-10 rounded-2xl w-[100%] h-[1px] absolute top-[1px]"></div>
+        <div className={`bg-white rounded-2xl w-[${`50%`}] h-[3px] absolute`}></div>
     </div>
 }
 export default BarTiming
