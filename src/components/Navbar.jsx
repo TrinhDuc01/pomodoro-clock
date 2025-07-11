6fe684eed02bdcaaf4d9301cda60c98a69d50d5f
TrinhDuc01 <ductrinh.0301@gmail.com>
2025-07-11 11:59:30 +0700

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,11 @@
 const NavBar = () => {
-    return <div>
-        <div>
-            <h1 className="bg-red-400">Pomodoro Clock</h1>
+    return <div className="flex items-center justify-between py-2">
+        <div className="logo flex items-center gap-1 py-2">
+            <i class="fa-regular fa-circle-check text-2xl"></i>
+            <h1 className="bg-red-400 text-2xl font-[500]">Pomodoro Clock</h1>
+        </div>
+        <div className="btn-group">
+            <button className="bg-[#ffffff33] opacity-90 px-3 py-2 rounded-md">Setting</button>
         </div>
     </div>
 }
