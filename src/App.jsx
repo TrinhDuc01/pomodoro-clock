2965d97f88b707c9d9677a5c7a3d6a9047dbf110
TrinhDuc01 <ductrinh.0301@gmail.com>
2025-07-11 11:48:18 +0700

    background

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Container from './components/Container'
 
 function App() {
   return (
-    <div className='bg-red-400 h-[100vh]'>
+    <div className='bg-red-400 h-[100vh] text-white'>
       <Container/>
     </div>
   )
