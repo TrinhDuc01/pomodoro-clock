6fcb39247727c88b1ad6f2aa92f3848108d1ef42
TrinhDuc01 <ductrinh.0301@gmail.com>
2025-07-14 12:18:41 +0700

    complete timer function

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,14 +1,30 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import Button from "./Button"
+import FormatTimeString from "../utilities/FormatTimeString"
 
 const Clock = () => {
     const dispatch = useDispatch()
     const time = useSelector(state => state.timeReducer)
     const [counter, setCounter] = useState(0)
+    const [start, setStart] = useState(false)
+    const timeOutIDRef = useRef(null)
     useEffect(() => {
-        setCounter
-    }, time)
+        setCounter(time.minutes * 60)
+        // console.log('set')
+    }, [time])
+
+    const handleStart = () => {
+        setStart(!start)
+        timeOutIDRef.current = setInterval(() => {
+            setCounter(prev => prev - 1)
+        }, 1000)
+    }
+
+    const handleStop = () => {
+        setStart(!start)
+        clearInterval(timeOutIDRef.current)
+    }
 
     const handleClock = (clock) => {
         dispatch({
@@ -16,7 +32,6 @@ const Clock = () => {
         })
     }
 
-    console.log(time)
     return <div className="flex flex-col justify-center items-center py-5 gap-6 rounded bg-[#ffffff33]">
         <div className="flex gap-2">
             <Button time={time} name={"Pomodoro"} handleClock={() => handleClock("POMODORO")} />
@@ -24,9 +39,13 @@ const Clock = () => {
             <Button time={time} name={"Long Break"} handleClock={() => handleClock("LONG")} />
         </div>
         <div className="text-6xl font-[500] text-[120px]">
-            25:00
+            {FormatTimeString(counter)}
         </div>
-        <button style={{color: time.color}} className={`bg-white hover:bg-gray-200 text-2xl px-12 py-3 mt-4 rounded font-[600]`}>START</button>
+        {
+            start ?
+                <button onClick={handleStop} style={{ color: time.color }} className={`bg-white hover:bg-gray-200 text-2xl px-12 py-3 mt-4 rounded font-[600]`}>STOP</button> :
+                <button onClick={handleStart} style={{ color: time.color }} className={`bg-white hover:bg-gray-200 text-2xl px-12 py-3 mt-4 rounded font-[600]`}>START</button>
+        }
     </div>
 }
 export default Clock
