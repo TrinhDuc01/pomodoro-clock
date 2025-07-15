c8310ff6dceb51b2f7feb4ec910f0002738c7541
TrinhDuc01 <ductrinh.0301@gmail.com>
2025-07-15 12:18:43 +0700

    rewrite timeReducer

--- src/redux/reducers/timeReducer.js
+++ src/redux/reducers/timeReducer.js
@@ -3,33 +3,47 @@ const time = [
         name: "Pomodoro",
         color: "#E55050",
         count: 1,
-        minutes: 25
+        minutes: 25,
+        message: "Time to focus!"
     },
     {
         name: "Short Break",
         color: "#4A9782",
         count: 1,
-        minutes: 5
+        minutes: 5,
+        message: "Time for a break!"
     },
     {
         name: "Long Break",
         color: "#00809D",
         count: 1,
-        minutes: 15
+        minutes: 15,
+        message: "Time for a break!"
     }
 ]
 
-const defaultState = time[0]
+const defaultState = {
+    modify: time,
+    currentTime: time[0],
+}
 
 
 const timeReducer = (state = defaultState, action) => {
     switch (action.type) {
         case "POMODORO":
-            return time[0];
+            return { ...state, currentTime: state.modify[0] };
         case "SHORT":
-            return time[1];
+            return { ...state, currentTime: state.modify[1] };
         case "LONG":
-            return time[2];
+            return { ...state, currentTime: state.modify[2] };
+        case "INCREASE_COUNT":
+            return {
+                modify: time.map(item => {
+                    if (item.name === action.payload.name) return { ...item, count: item.count + 1 }
+                    return item
+                }),
+                currentTime: { ...state.currentTime, count: state.currentTime.count + 1 }
+            }
         default:
             return state
     }
