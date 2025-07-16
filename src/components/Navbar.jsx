6ed73bec82787322683cca5b6c440b734b756cc0
TrinhDuc01 <ductrinh.0301@gmail.com>
2025-07-16 19:34:39 +0700

    Sua navbar cho chuc nang Thay doi thoi gian

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,16 @@
+import { useState } from "react"
+import ModalSetting from "./ModalSetting"
+
 const NavBar = () => {
+    const [isOpen, setIsOpen] = useState(false)
     return <div className="flex items-center justify-between py-2">
         <div className="logo flex items-center gap-1 py-2">
             <i class="fa-regular fa-circle-check text-2xl"></i>
             <h1 className="text-2xl font-[500]">Pomodoro Clock</h1>
         </div>
         <div className="btn-group">
-            <button className="bg-[#ffffff33] opacity-90 px-3 py-2 rounded-md"><i class="fa-solid fa-gear inline-block mr-1"></i>Setting</button>
+            <button onClick={() => setIsOpen(true)} className="bg-[#ffffff33] hover:bg-[#00000026] opacity-90 px-3 py-2 rounded-md"><i class="fa-solid fa-gear inline-block mr-1"></i>Setting</button>
+            {isOpen && <ModalSetting isOpen={isOpen} setIsOpen={setIsOpen} />}
         </div>
     </div>
 }
